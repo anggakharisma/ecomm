647b1c09c50b97bff9a439f80fca3dc3f48e38ad
angga kharisma <anggaputrakharisma@gmail.com>
2023-03-07 22:06:58 +0700

    fix: formatting

--- components/ProductShowcase.vue
+++ components/ProductShowcase.vue
@@ -1,8 +1,9 @@
 <template>
 	<div class="w-full flex justify-center align-middle items-center
-		cursor-pointer relative group">
+			cursor-pointer relative group">
 		<h3 @click="addItemToCart" class="text-lg tracking-wide absolute z-30 bottom-1/4 opacity-0
-		group-hover:opacity-100 transition-all bg-white p-2 hover:bg-red-400 ounded-full hover:text-white hover:px-6">+ CART</h3>
+			group-hover:opacity-100 transition-all bg-white p-2 hover:bg-red-400 ounded-full hover:text-white hover:px-6">+ CART
+		</h3>
 		<img class="w-[22.5vw] lg:w-1/2 group-hover:scale-110 transition-all" :src="useAssets(`${props.image}`)"
 			alt="bottle banana" />
 	</div>
@@ -11,7 +12,7 @@
 <script setup lang="ts">
 import { cartStore } from '../store/cart';
 import { notificationStore } from '../store/notification';
-import useAssets  from '../composable/useAssets'
+import useAssets from '../composable/useAssets'
 
 
 const props = defineProps<{
@@ -31,7 +32,7 @@ const addItemToCart = () => {
 	});
 
 	notificationStore.value.showNotification(`${props.productName} Added`);
-	
+
 }
 
 </script>
